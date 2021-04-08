3bd6eff37ba88edbb9292ae8de3e48485a98dd14
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-07 21:13:27 -0500

    created generate page and added page-template.js

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+/*
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++) {
         console.log(profileDataArr[i]);
@@ -13,3 +36,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
