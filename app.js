da464ddfcf1331d46ce0e125c87d841d19540163
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-07 22:25:17 -0500

    installed inquirer and initialized prompt

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
 
+// const fs = require('fs');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
+// const generatePage = require('./src/page-template.js');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+    // if(err) throw err;
+
+    // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
